095ffb423ef01400f26443aee3499b0facd8c32d
Aananth C N <c.n.aananth@gmail.com>
2024-01-07 22:36:00 +0530

    Removed FIFO logic and therefore removed it in mk file

--- tools/os_builder/src/os-objs.mk
+++ tools/os_builder/src/os-objs.mk
@@ -11,9 +11,6 @@ SG_OBJS	:= \
 	${OS_BUILDER_PATH}/src/sg_events.o \
 	${OS_BUILDER_PATH}/src/sg_resources.o \
 	${OS_BUILDER_PATH}/src/sg_messages.o \
-	${OS_BUILDER_PATH}/src/sg_fifo.o \
 	${OS_BUILDER_PATH}/src/sg_alarms.o \
 	${OS_BUILDER_PATH}/src/sg_tasks.o \
 	${OS_BUILDER_PATH}/src/sg_ivector.o
-
-
