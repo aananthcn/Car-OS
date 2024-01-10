6790f70b3d360ebd2c6fe4f96f086d3edf389725
Aananth C N <c.n.aananth@gmail.com>
2024-01-10 12:32:17 +0530

    Printf issue and general cleanup

--- boards/rp2040/board.h
+++ boards/rp2040/board.h
@@ -6,8 +6,8 @@
 
 #define BOARD_RP2040	1
 
-int bsp_setup_sytimer(void);
-int bsp_get_usec_syscount(u32 *ucount);
-int bsp_mcu_startup(void);
+// int bsp_setup_sytimer(void);
+// int bsp_get_usec_syscount(u32 *ucount);
+// int bsp_mcu_startup(void);
 
 #endif
